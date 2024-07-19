a7260e598676ecfb5defc7b120172fbae3e5c555
Leonardo Viana <leonardo.viana@grupomateus.com>
2024-07-19 18:36:27 -0300

    feat(product-api): add endpoint health check

--- app/product-api/src/main/java/io/viana/product_api/StatusController.java
+++ app/product-api/src/main/java/io/viana/product_api/StatusController.java
@@ -6,17 +6,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 
 @RestController
 @RequestMapping("/api")
 public class StatusController {
 
+    @Value("${spring.app.name}")
+    private String webApplicationName;
+
     @GetMapping("status")
-    public ResponseEntity<HashMap<String, Object>> getApiStatus(){
+    public ResponseEntity<HashMap<String, Object>> getApiStatus() {
         var response = new HashMap<String, Object>();
-        response.put("service","Product-API");
-        response.put("status","up");
-        response.put("httpStatus",200);
+        response.put("service", "Product-API");
+        response.put("status", "up");
+        response.put("httpStatus", 200);
         return ResponseEntity.ok(response);
     }
 }
